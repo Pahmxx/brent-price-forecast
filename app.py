bdf73a8303f37160db05626381b59e4da8940b05
Phaola Oliveira Silva <79982018+Pahmxx@users.noreply.github.com>
2024-12-02 20:37:03 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+def main():
+    # Configuração da página
+    st.set_page_config(page_title='MVP para análise temporal de petróleo',
+                       page_icon='🛢️')
+
+    st.write('# MVP para análise de preço do petróleo Brent')
